9e32ecd3ae8c7e544841ecf1e74ab129f9c65260
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-20 14:06:14 -0300

    remove primevue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import PrimeVue from 'primevue/config'
-import Lara from './presets/lara'
 
-createApp(App).use(PrimeVue, { unstyled: true, pt: Lara }).mount('#app')
+createApp(App).mount('#app')
