5cdb83e20ad43edff4e6304facc9266b2a235917
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-19 23:23:43 -0300

    primevue config added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Lara from './presets/lara'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue, { unstyled: true, pt: Lara }).mount('#app')
