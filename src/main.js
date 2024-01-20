0f9c851d77f1eb9db197e04369398ba365024f3e
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-20 16:51:14 -0300

    vue-icons added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { OhVueIcon, addIcons } from "oh-vue-icons"
+import { WiCloudyWindy, WiHumidity, WiThermometer, WiShowers } from "oh-vue-icons/icons"
+addIcons(WiCloudyWindy, WiHumidity, WiThermometer, WiShowers)
 
-createApp(App).mount('#app')
+createApp(App).component("v-icon", OhVueIcon).mount('#app')
