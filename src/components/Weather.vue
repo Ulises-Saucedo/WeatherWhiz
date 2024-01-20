e9e007d7cbf2bd729ffcce5479dee8ebcb3d50b7
Ulises Saucedo <ulisesaucedo.se04@gmail.com>
2024-01-19 23:25:02 -0300

    Weather\main component added

---
+++ src/components/Weather.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import WeatherProvider from '../providers/weather.js'
+
+    const weatherRef = new WeatherProvider()
+    const weather = weatherRef.city
+
+    await weatherRef.getCity('London')
+</script>
+
+<template>
+    <h2>{{ weather.name }}</h2>
+</template>
